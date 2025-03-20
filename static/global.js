69ea39a5048d1f81467df53ec5619a6f7e760223
Kauan <kauan.mariani@gmail.com>
2025-03-20 12:38:12 -0300

    mudando o jeito

--- static/global.js
+++ static/global.js
@@ -19,9 +19,9 @@
 const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
 let pages = [
-    {url: "", title : "Home"},
-    {url: "projects/", title : "Projects"},
-    {url: "contact/", title : "Contact"},
+    {url: "./", title : "Home"},
+    {url: "projects", title : "Projects"},
+    {url: "contact", title : "Contact"},
     {url: "https://github.com/kauanmaf", title : "GitHub"}
 
 ];
