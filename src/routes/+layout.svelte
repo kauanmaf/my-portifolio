0148b530e1d1c9cb4eeb174e31dfa211c0e77571
Kauan <kauan.mariani@gmail.com>
2025-03-27 11:42:28 -0300

    adicionando svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+
+<script>
+    import { base } from "$app/paths";
+    let pages = [
+    {url: "/", title : "Home"},
+    {url: "/projects", title : "Projects"},
+    {url: "/contact", title : "Contact"},
+    {url: "https://github.com/kauanmaf", title : "GitHub"}
+];
+</script>
+
+<nav>
+    {#each pages as p}
+        <a href= { p.url.startsWith("http")? p.url: `${base}${p.url}`}>{p.title}</a>
+    {/each}
+</nav>
+
+  
+
+<slot />
--- static/global.js
+++ static/global.js
@@ -16,40 +16,40 @@
 // currentLink?.classList.add("current");
 // console.log(navLinks);
 
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
+// const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-let pages = [
-    {url: "./", title : "Home"},
-    {url: "projects", title : "Projects"},
-    {url: "contact", title : "Contact"},
-    {url: "https://github.com/kauanmaf", title : "GitHub"}
-];
+// let pages = [
+//     {url: "./", title : "Home"},
+//     {url: "projects", title : "Projects"},
+//     {url: "contact", title : "Contact"},
+//     {url: "https://github.com/kauanmaf", title : "GitHub"}
+// ];
 
 
-let nav = document.createElement("nav");
-document.body.prepend(nav);
+// let nav = document.createElement("nav");
+// document.body.prepend(nav);
 
-for (let p of pages) {
-    console.log(p)
-    let url = p.url;
-    // if (!ARE_WE_HOME && !url.startsWith("http")) {
-    //     url = "../" + url;
-    // }
-    let title = p.title;
+// for (let p of pages) {
+//     console.log(p)
+//     let url = p.url;
+//     // if (!ARE_WE_HOME && !url.startsWith("http")) {
+//     //     url = "../" + url;
+//     // }
+//     let title = p.title;
     
-    // Create link and add it to nav
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
+//     // Create link and add it to nav
+//     let a = document.createElement("a");
+//     a.href = url;
+//     a.textContent = title;
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add("current");
+//     }
     
-    if (a.host != location.host)
-        {
-            a.target = "_blank";
-        }
+//     if (a.host != location.host)
+//         {
+//             a.target = "_blank";
+//         }
 
-    nav.append(a);
-}
+//     nav.append(a);
+// }
 
