0cc93c8e0bccc10310d366151900ef27672309d1
Kauan <kauan.mariani@gmail.com>
2025-03-27 12:27:36 -0300

    adicionando temas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,95 @@
 
+<!-- Java script -->
 <script>
     import { base } from "$app/paths";
+    import { page } from "$app/stores";
+
     let pages = [
     {url: "/", title : "Home"},
     {url: "/projects", title : "Projects"},
     {url: "/contact", title : "Contact"},
     {url: "https://github.com/kauanmaf", title : "GitHub"}
 ];
+
+let localStorage = globalThis.localStorage ?? {};
+
+let colorScheme = localStorage.colorScheme ?? "light";
+
+let root = globalThis?.document?.documentElement;
+$: root?.style.setProperty("color-scheme", colorScheme);
+$: localStorage.colorScheme = colorScheme;
+
+let profileData = fetch("https://api.github.com/users/kauanmaf");
+
+
 </script>
 
 <nav>
     {#each pages as p}
-        <a href= { p.url.startsWith("http")? p.url: `${base}${p.url}`}>{p.title}</a>
+    <a
+        href={p.url.startsWith("http") ? p.url: `${base}${p.url}`}
+        class:current={$page.route.id === p.url}
+        target={p.url.startsWith("http") ? "_blank" : undefined}
+    >
+        {p.title}
+    </a>
     {/each}
 </nav>
 
-  
+<label class="color-scheme">
+    Theme:
+
+    <select bind:value={ colorScheme }>
+
+        <option value="light dark"> Automatic </option>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+    </select>
+</label>
+
+
+
+<!-- CSS -->
+<style>
+    nav{
+	display: flex;
+	border-bottom-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-color: var(--grey);
+}
+
+nav a{
+	flex: 1;
+	text-decoration: none;
+	color: inherit;
+	text-align : center;
+	padding: 0.5cm;
+}
+
+nav .current{
+	border-bottom-width: 0.4em;
+	border-bottom-style: solid;
+	border-bottom-color: var(--grey);
+	padding: -0.5em;
+}
+a:hover{
+	border-bottom-width: 0.4em;
+	border-bottom-style: solid;
+	border-bottom-color: var(--color-accent);
+	padding-bottom: -0.1em;
+	background-color: var(--color-accent-bg);
+
+}
+
+/* <label class="color-scheme">
+    Theme:
+    <select>
+        <option value="light dark"> Automatic </option>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+    </select>
+</label> */
+
+</style>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<svelte:head>
+  <title>Homepage</title>
+</svelte:head>
 <!-- <h1> (Heading 1) represents the main heading of a webpage or a section.-->
 <h1> Kauan Mariani Ferreira</h1>
 
@@ -10,3 +13,53 @@
         <img src="images/my_image.jpeg" alt="A description of the image">
     -->
         <img src="images/download.jpeg" alt="Shrek" height=400>
+
+        {#await fetch("https://api.github.com/users/kauanmaf")}
+  <p>Loading...</p>
+{:then response}
+  {#await response.json()}
+    <p>Decoding...</p>
+  {:then data} 
+    <section>
+      <h2>My Github Stats</h2>
+      <dl>
+        <dt>Followers</dt>
+        <dd>{data.followers}</dd>
+        <dt>Following</dt>
+        <dd>{data.following}</dd>
+        <dt>Public Repos</dt>
+        <dd>{data.public_repos}</dd>
+      </dl>
+    </section>
+  {:catch error}
+    <p class="error">Something went wrong: {error.message}</p>
+  {/await}
+  {:catch error}
+    <p class="error">Something went wrong: {error.message}</p>
+{/await}
+
+<style>
+    dl{
+    display: grid;
+    grid-template-columns: auto;
+  }
+  dt{
+    grid-row: 1;
+    font-family: inherit;
+    font-weight: bold;
+    color: var(--border-gray);
+    text-transform: uppercase;
+  }
+  dd{
+    font-family: inherit;
+    font-weight: bold;
+  }
+  section{
+    border-width:0.15em;
+  	border-style:solid;
+	  border-color:var(--border-gray);
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+
+</style>
